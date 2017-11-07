ca1f230e5794380ede9138311bfd5879e39c8d85
dongsj <232171773@qq.com>
2017-11-07 17:10:00 +0800

    Getting Input From User

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const _ = require('lodash');
 
 const notes = require('./notes');
 
+let command = process.argv[2];
+console.log(process.argv);
+console.log(`Command:${command}`);
+
+if (command === 'add') {
+    console.log('Adding new note');
+}else if (command==='list'){
+    console.log('Listing all notes');
+}else if (command==='read'){
+    console.log('Reading notes');
+}else if (command==='remove'){
+    console.log('Removing notes');
+}else{
+    console.log('Command no recognized');
+}
