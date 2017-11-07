f7f72cf7076192b13351d08be0bcbf8e8c751ff7
dongsj <232171773@qq.com>
2017-11-07 17:57:19 +0800

    Simplified Input With Yargs

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
 
-let command = process.argv[2];
-console.log(process.argv);
-console.log(`Command:${command}`);
+const argv = yargs.argv;
+console.log('yargs:'+JSON.stringify(argv));
+
+let command = argv._[0];
+// console.log(process.argv);
+// console.log(`Command:${command}`);
 
 if (command === 'add') {
     console.log('Adding new note');
-}else if (command==='list'){
+    notes.addNote(argv.title,argv.body);
+} else if (command === 'list') {
     console.log('Listing all notes');
-}else if (command==='read'){
+} else if (command === 'read') {
     console.log('Reading notes');
-}else if (command==='remove'){
+} else if (command === 'remove') {
     console.log('Removing notes');
-}else{
+} else {
     console.log('Command no recognized');
 }
--- notes.js
+++ notes.js
@@ -1,1 +1,8 @@
 console.log('Starting note.js');
+
+let addNote = (title, body) => {
+    console.log('Adding note',title,body);
+};
+module.exports = {
+    addNote
+};
