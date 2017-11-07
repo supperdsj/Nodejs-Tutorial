75f331e95d74dd60299d8e62d9a8b6a3bb1964bd
dongsj <232171773@qq.com>
2017-11-07 16:50:01 +0800

    Using 3rd Party Modules

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
 let res = notes.addNote();
 console.log(res);
 console.log('add(1,-19) result:' + notes.add(1, -19));
+
+console.log(`123 is string? ${_.isString(123)}`);
+console.log(`abc is string? ${_.isString('abc')}`);
+
+console.log(`5,1,2,3,4,5,1,2,3 uniq: ${_.uniq([5,1,2,3,4,5,1,2,3])}`);
 
 const userInfo = os.userInfo();
 fs.appendFile('greetings.txt', `Hello ${userInfo.username}! Age ${notes.age}\n`, (err) => {
