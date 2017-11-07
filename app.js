53c4f33aefb688d8ba9be824264fc77dea46098f
dongsj <232171773@qq.com>
2017-11-07 16:56:46 +0800

    Restarting App with Nodemon

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes');
 const _ = require('lodash');
 
-let res = notes.addNote();
-console.log(res);
-console.log('add(1,-19) result:' + notes.add(1, -19));
-
-console.log(`123 is string? ${_.isString(123)}`);
-console.log(`abc is string? ${_.isString('abc')}`);
-
-console.log(`5,1,2,3,4,5,1,2,3 uniq: ${_.uniq([5,1,2,3,4,5,1,2,3])}`);
-
-const userInfo = os.userInfo();
-fs.appendFile('greetings.txt', `Hello ${userInfo.username}! Age ${notes.age}\n`, (err) => {
-    if (err) {
-        console.log('append file error');
-        throw err;
-    } else {
-        console.log('append file success')
-    }
-});
+const notes = require('./notes');
 
--- notes.js
+++ notes.js
@@ -1,16 +1,1 @@
 console.log('Starting note.js');
-// module.exports.age = 27;
-// module.exports.addNote = () => {
-//   console.log('addNote');
-//   return 'newNote';
-// };
-module.exports = {
-    age: 27,
-    addNote() {
-        console.log('addNote');
-        return 'newNote';
-    },
-    add(a, b) {
-        return a + b;
-    }
-};
