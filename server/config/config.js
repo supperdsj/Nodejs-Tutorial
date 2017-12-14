8bf8f02ad4532cda896fc3113accd592be58c750
dongsj <232171773@qq.com>
2017-12-14 17:30:14 +0800

    add env for db

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+let env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+console.log('env **** ',env);
+console.log('PORT **** ',process.env.PORT);
+console.log('MONGODB_URI **** ',process.env.MONGODB_URI);
