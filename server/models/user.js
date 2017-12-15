dbab1866033188247affe814a7df46da29251c58
dongsj <232171773@qq.com>
2017-12-15 14:45:46 +0800

    update user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,37 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-let User = mongoose.model('User', {
+const User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            // validator:(value)=>{
+            //     return validator.isEmail(value);
+            // },
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -85,6 +85,7 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
