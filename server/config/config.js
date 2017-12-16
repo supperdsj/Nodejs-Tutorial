fe6164de0198ce590a89e2a7bd298a1f189252c1
dongsj <232171773@qq.com>
2017-12-17 00:15:09 +0800

    disable Opbeat

--- server/config/config.js
+++ server/config/config.js
@@ -5,9 +5,10 @@ if (env === 'development') {
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if (env === production){
-    const opbeat = require('opbeat').start();
 }
+// else if (env === production){
+//     const opbeat = require('opbeat').start();
+// }
 console.log('env **** ',env);
 console.log('PORT **** ',process.env.PORT);
 console.log('MONGODB_URI **** ',process.env.MONGODB_URI);
