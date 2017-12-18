a47db1d6f70be443a860083e4f54a8aa79098218
dongsj <232171773@qq.com>
2017-12-18 11:43:30 +0800

    add user finish

--- server/server.js
+++ server/server.js
@@ -85,6 +85,15 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+    user.save().then((user) => {
+        res.send({user});
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
