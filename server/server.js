6a068b36d256909e3b18924360f59493f95cd1fb
dongsj <232171773@qq.com>
2017-12-18 15:51:15 +0800

    login finish

--- server/server.js
+++ server/server.js
@@ -111,6 +111,17 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
+app.post('/users/login',(req,res)=>{
+    const body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email,body.password).then((user)=>{
+        user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e)=>{
+        res.status(401).send()
+    });
+});
+
 
 app.get('/error', (req, res) => {
     throw 'test Error';
