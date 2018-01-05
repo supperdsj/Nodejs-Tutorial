fa34f6956ad1c840743cd2297aa5aeae8d5d8065
dongsj <232171773@qq.com>
2018-01-05 18:24:12 +0800

    enable send and get message with html

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connect to server');
-    socket.emit('createMessage', {
-        from: 'dongsj@qq.com',
-        text: 'nice to mt you',
-        to: 'wx@qq.com',
-        createAt: +new Date()
-    });
 });
 
 socket.on('disconnect', function () {
@@ -15,5 +9,18 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('new message', message);
+    $('#messages').append('<li>' + message.from + ':' + message.text + '</li>');
+});
+
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    if ($('input[name=message]').val().trim() !== '') {
+        socket.emit('createMessage', {
+            from: 'User',
+            text: $('input[name=message]').val()
+        }, function (str) {
+        });
+    } else {
+        alert('please input some message');
+    }
 });
