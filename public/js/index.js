547c49baa6b45af8dbaa5072d380aafea3dccd98
dongsj <232171773@qq.com>
2018-01-08 18:42:54 +0800

    use mustache to resolve the templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a');
-    $('#messages').append('<li>' + message.from + ' [' + formattedTime + '] : ' + message.text + '</li>');
+    // $('#messages').append('<li>' + message.from + ' [' + formattedTime + '] : ' + message.text + '</li>');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createAt:formattedTime
+    });
+    $('#messages').append(html);
 });
 socket.on('newLocation', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a');
-    $('#messages').append('<li>' + message.from + ' [' + formattedTime + '] : ' + '<a target="_blank" href="' + message.url + '">' + 'shared location' + '</a></li>');
+    // $('#messages').append('<li>' + message.from + ' [' + formattedTime + '] : ' + '<a target="_blank" href="' + message.url + '">' + 'shared location' + '</a></li>');
+
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template,{
+        url:message.url,
+        from:message.from,
+        createAt:formattedTime
+    });
+    $('#messages').append(html);
 });
 
 $('#message-form').on('submit', function (e) {
