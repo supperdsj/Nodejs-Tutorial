31673a74d1118273d7f29aaed92065f6a6972c4c
dongsj <232171773@qq.com>
2018-01-08 18:53:28 +0800

    add autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHegiht = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHegiht + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connect to server');
 });
@@ -12,38 +27,40 @@ socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a');
     // $('#messages').append('<li>' + message.from + ' [' + formattedTime + '] : ' + message.text + '</li>');
     var template = $('#message-template').html();
-    var html = Mustache.render(template,{
-        text:message.text,
-        from:message.from,
-        createAt:formattedTime
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 socket.on('newLocation', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a');
     // $('#messages').append('<li>' + message.from + ' [' + formattedTime + '] : ' + '<a target="_blank" href="' + message.url + '">' + 'shared location' + '</a></li>');
 
     var template = $('#location-message-template').html();
-    var html = Mustache.render(template,{
-        url:message.url,
-        from:message.from,
-        createAt:formattedTime
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
-    if ($('input[name=message]').val().trim() !== '') {
+    // if ($('input[name=message]').val().trim() !== '') {
         socket.emit('createMessage', {
             from: 'User',
             text: $('input[name=message]').val()
         }, function (str) {
             $('input[name=message]').val('');
         });
-    } else {
-        alert('please input some message');
-    }
+    // } else {
+    //     alert('please input some message');
+    // }
 });
 
 $('#share-location').on('click', function (e) {
